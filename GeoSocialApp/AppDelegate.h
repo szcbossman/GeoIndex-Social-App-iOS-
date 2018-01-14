68029b801bc462c6240661ee3e4b2f550b025422
szcbossman <songzicheng3@gmail.com>
2018-01-13 16:46:13 -0800

    App proj created

---
+++ GeoSocialApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeoSocialApp
+//
+//  Created by song zicheng daily use on 2018/1/13.
+//  Copyright © 2018年 Troy Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
