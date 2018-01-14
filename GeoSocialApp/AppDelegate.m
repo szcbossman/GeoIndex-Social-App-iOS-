0a214c22e36f9c1057c5df52e7a20d4b1b130157
szcbossman <songzicheng3@gmail.com>
2018-01-13 17:26:52 -0800

    add TabViewController

--- GeoSocialApp/AppDelegate.m
+++ GeoSocialApp/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "SCTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    SCTabBarController *tabBarVC = [SCTabBarController new];
+    self.window.rootViewController = tabBarVC;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
--- GeoSocialApp/SCTabBarController.m
+++ GeoSocialApp/SCTabBarController.m
@@ -18,19 +18,34 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.viewControllers = [self viewControllerArray];
+    self.selectedIndex = 0;
 }
 
+- (NSArray <UIViewController *> *)viewControllerArray {
+    UIViewController *homeController = [self homeViewController];
+    UIViewController *exploreController = [self exploreViewController];
+    NSArray<UIViewController *> *array = @[homeController, exploreController];
+    return array;
+}
 
+- (UIViewController *)homeViewController {
+    UIViewController *homeController = [[UIViewController alloc] init];
+    homeController.view.backgroundColor = [UIColor whiteColor];
+    homeController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Home" image:[UIImage imageNamed:@"Events"] selectedImage:[UIImage imageNamed:@"Events_selected"]];
+    homeController.tabBarItem.tag = 0;
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:homeController];
+    return navigationController;
+}
 
-
-
-
-
-
-
-
-
-
+- (UIViewController *)exploreViewController {
+    UIViewController *exploreController = [[UIViewController alloc] init];
+    exploreController.view.backgroundColor = [UIColor blueColor];
+    exploreController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Explore" image:[UIImage imageNamed:@"Explore"] selectedImage:[UIImage imageNamed:@"Explore_selected"]];
+    exploreController.tabBarItem.tag = 1;
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:exploreController];
+    return navigationController;
+}
 
 
 
