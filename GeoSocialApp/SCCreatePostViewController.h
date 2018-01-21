bba3759690a0b96aae742908e7a4b49c772875f5
szcbossman <songzicheng3@gmail.com>
2018-01-21 15:17:38 -0800

    SCCreatePostViewCtrler added

---
+++ GeoSocialApp/SCCreatePostViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCCreatePostViewController.h
+//  GeoSocialApp
+//
+//  Created by song zicheng daily use on 2018/1/21.
+//  Copyright © 2018年 Troy Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCCreatePostViewController : UIViewController
+
+@end
---
+++ GeoSocialApp/SCCreatePostViewController.m
@@ -0,0 +1,40 @@
+//
+//  SCCreatePostViewController.m
+//  GeoSocialApp
+//
+//  Created by song zicheng daily use on 2018/1/21.
+//  Copyright © 2018年 Troy Song. All rights reserved.
+//
+
+#import "SCCreatePostViewController.h"
+
+@interface SCCreatePostViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *postTextView;
+@property (weak, nonatomic) IBOutlet UIImageView *selectedImageView;
+
+@end
+
+@implementation SCCreatePostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
