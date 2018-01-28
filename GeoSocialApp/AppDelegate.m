c324c8c509078bd1e9625d557aac4e6798ccbd46
szcbossman <songzicheng3@gmail.com>
2018-01-28 12:18:36 -0800

    location service configured in AppDelegate

--- GeoSocialApp/AppDelegate.m
+++ GeoSocialApp/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "SCTabBarController.h"
+#import "SCLocationManager.h"
 
 @interface AppDelegate ()
 
@@ -22,6 +23,7 @@
     SCTabBarController *tabBarVC = [SCTabBarController new];
     self.window.rootViewController = tabBarVC;
     [self.window makeKeyAndVisible];
+    [self updateLocation];
     return YES;
 }
 
@@ -35,11 +37,13 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[SCLocationManager sharedManager] stopLoadUserLocation];
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    [self updateLocation];
 }
 
 
@@ -52,5 +56,12 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)updateLocation
+{
+    if ([SCLocationManager isLocationServicesEnabled]) {
+        SCLocationManager *locationManager  = [SCLocationManager sharedManager];
+        [locationManager startLoadUserLocation];
+    }
+}
 
 @end
